d982cd1e9ae40c1f0223f03d783c4fabd1ca5847
Marc Jakobi <marc.jakobi@tiko.energy>
2024-03-13 18:02:52 +0100

    build: add luarocks-build-treesitter-parser to flake packages outputs

--- flake.nix
+++ flake.nix
@@ -83,6 +83,8 @@
             ]);
         };
 
+        packages.default = pkgs.lua51Packages.luarocks-build-treesitter-parser;
+
         legacyPackages.fixtures = {
           inherit
             (pkgs.lua51Packages)
