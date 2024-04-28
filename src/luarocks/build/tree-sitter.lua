82e1100e9f202e19896ec97b80d98b1024c04e29
Marc Jakobi <marc.jakobi@tiko.energy>
2024-04-28 20:39:38 +0200

    feat!: remove tree-sitter build script backward compatibility adapter

--- src/luarocks/build/tree-sitter.lua
+++
@@ -1,4 +0,0 @@
--- Sometimes, luarocks will automatically search for a rock called "luarocks-build-<build.type>",
--- so the build type had to be changed to treesitter-parser.
--- This module ensures that working rockspecs that use "tree-sitter" as a build type still work.
-return require("luarocks.build.treesitter-parser")
